1f4fc81f058381990acd79ff6c62f050e9600f2a
unknown <luvumbu.n@gmail.com>
2020-03-24 12:32:04 +0100

    ajout en commentaire la basse du code  brute

--- index.php
+++ index.php
@@ -29,16 +29,60 @@ if(!empty($fichier)){
                     {
                         $valeurf = $valeurf.$lien[$i] ; 
                     }
-            }         
+            }     
+                
             if($valeurf=="tiff" || $valeurf=="jpg" || $valeurf=="gif" || $valeurf=="png")
             {            
                 ?>
+
                 <img src="<?php echo $lien ?>" alt="" srcset="">
                 <?php
             }            
         } 
  }
+
+
+ /*
+
+ <?php
+$dir_nom = '.'; // dossier listé (pour lister le répertoir courant : $dir_nom = '.'  --> ('point')
+$dir = opendir($dir_nom) or die('Erreur de listage : le répertoire n\'existe pas'); // on ouvre le contenu du dossier courant
+$fichier= array(); // on déclare le tableau contenant le nom des fichiers
+$dossier= array(); // on déclare le tableau contenant le nom des dossiers
+ 
+while($element = readdir($dir)) {
+    if($element != '.' && $element != '..') {
+        if (!is_dir($dir_nom.'/'.$element)) {$fichier[] = $element;}
+        else {$dossier[] = $element;}
+    }
+}
+ 
+closedir($dir);
+ 
+if(!empty($dossier)) {
+    sort($dossier); // pour le tri croissant, rsort() pour le tri décroissant
+    echo "<p>Liste des dossiers accessibles dans '$dir_nom' : \n\n</p>";
+    echo "\t\t<ul class=rien>\n";
+        foreach($dossier as $lien){
+            echo "\t\t\t<li><a href=\"$dir_nom/$lien \">$lien</a></li>\n";
+        }
+    echo "\t\t</ul>";
+}
+ 
+if(!empty($fichier)){
+    sort($fichier);// pour le tri croissant, rsort() pour le tri décroissant
+    echo "<p>Liste des images accessibles sur l'hebergeur : \n\n</p>";
+    echo "\t\t<ul class=rien>\n";
+        foreach($fichier as $lien) {
+            echo "\t\t\t<li><img src=\"$dir_nom/$lien \"></li>\n";
+        }
+    echo "\t\t</ul>";
+ }
 ?>
+
+*/
+?>
+
 <style>
 img 
 {
